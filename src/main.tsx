ed990e019552b42767c9c28b9c89b39f853049df
Ali Bahaari <alibahaari1997@gmail.com>
2024-12-14 19:09:08 +0330

    chore: configured libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import WebApp from "@twa-dev/sdk";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 WebApp.ready();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +10,9 @@ export default defineConfig({
     outDir: "./docs",
   },
   base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
