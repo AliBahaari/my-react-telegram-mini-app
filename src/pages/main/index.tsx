9404b761dd85850d2ef2b2612a2c277664dc5585
ferizoozoo <farhadzohoorz@gmail.com>
2024-12-18 20:53:51 +0330

    fix: Changing the style of button div components at the bottom of the screen of mates and main pages

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 function Main() {
   return (
     <Default>
-      <div className="flex flex-col gap-y-2 my-2">
+      <div className="flex flex-col gap-y-2">
         <div className="flex flex-col gap-y-2">
           <Button>Invest on TrueDapp</Button>
           <Button>Buy TGM xx% below estimated listing price</Button>
@@ -15,7 +15,7 @@ function Main() {
         </div>
         <Button className="h-24 my-2 rounded-none">Fairytale Program</Button>
 
-        <div className="flex flex-row w-full absolute bottom-0">
+        <div className="flex flex-row w-full">
           <Button className="rounded-none w-1/4">Mates</Button>
           <Button className="rounded-none w-1/4">Tasks</Button>
           <Button className="rounded-none w-1/4">Ranking</Button>
