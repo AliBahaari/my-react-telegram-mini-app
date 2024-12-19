be1e79180c4f7c79eafddd76b5680385e68ec789
ferizoozoo <farhadzohoorz@gmail.com>
2024-12-19 20:02:23 +0330

    feat: add navigation on stats, wallet and support buttons of Header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,35 @@
+import { useNavigate } from "react-router";
 import { Button } from "../ui/button";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const onStatsClick = () => {
+    navigate("/stats");
+  };
+
+  const onWalletClick = () => {
+    navigate("/wallet");
+  };
+
+  const onSupportClick = () => {
+    window.location.href = "https://google.com";
+  };
+
   return (
     <div className="flex gap-2 flex-col">
       <div className="flex flex-row flex-wrap justify-between">
         <Button className="rounded-none">Logo</Button>
         <Button className="rounded-none">Level</Button>
-        <Button className="rounded-none">Stats</Button>
-        <Button className="rounded-none">Wallet</Button>
-        <Button className="rounded-none">Support</Button>
+        <Button className="rounded-none" onClick={onStatsClick}>
+          Stats
+        </Button>
+        <Button className="rounded-none" onClick={onWalletClick}>
+          Wallet
+        </Button>
+        <Button className="rounded-none" onClick={onSupportClick}>
+          Support
+        </Button>
       </div>
       <div className="text-center p-3 border-2 border-black">
         <span>xxxxxx TGM = xxxxx USDT</span>
