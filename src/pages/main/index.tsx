a4b907c63f584ed26ce8c158fe15dc49ddf2da29
ferizoozoo <farhadzohoorz@gmail.com>
2024-12-20 08:51:38 +0330

    feat: add navigation on invest, buy, mates, tasks and ranking buttons of main

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,38 @@
 import Default from "@/components/layout/default";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
 
 function Main() {
+  const navigate = useNavigate();
+
+  const onInvestOnTrueDappClick = () => {
+    navigate("/invest-on-truedapp");
+  };
+
+  const onBuyClick = () => {
+    navigate("/buy-tgm");
+  };
+
+  const onTasksClick = () => {
+    navigate("/tasks");
+  };
+
+  const onRankingClick = () => {
+    navigate("/ranking");
+  };
+
+  const onMatesClick = () => {
+    navigate("/mates");
+  };
+
   return (
     <Default>
       <div className="flex flex-col gap-y-2">
         <div className="flex flex-col gap-y-2">
-          <Button>Invest on TrueDapp</Button>
-          <Button>Buy TGM xx% below estimated listing price</Button>
+          <Button onClick={onInvestOnTrueDappClick}>Invest on TrueDapp</Button>
+          <Button onClick={onBuyClick}>
+            Buy TGM xx% below estimated listing price
+          </Button>
         </div>
         <div className="flex flex-row">
           <Button className="flex-1 h-64 rounded-none">Cash Avalanche</Button>
@@ -16,9 +41,15 @@ function Main() {
         <Button className="h-24 my-2 rounded-none">Fairytale Program</Button>
 
         <div className="flex flex-row w-full">
-          <Button className="rounded-none w-1/4">Mates</Button>
-          <Button className="rounded-none w-1/4">Tasks</Button>
-          <Button className="rounded-none w-1/4">Ranking</Button>
+          <Button className="rounded-none w-1/4" onClick={onMatesClick}>
+            Mates
+          </Button>
+          <Button className="rounded-none w-1/4" onClick={onTasksClick}>
+            Tasks
+          </Button>
+          <Button className="rounded-none w-1/4" onClick={onRankingClick}>
+            Ranking
+          </Button>
           <Button className="rounded-none w-1/4">Reserved</Button>
         </div>
       </div>
